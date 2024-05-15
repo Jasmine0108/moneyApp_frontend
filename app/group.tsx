d037a8d5300e498bacee16c23ea69281b9c93d2e
Jasmine <a0905373664@gmail.com>
2024-05-15 21:50:23 +0800

    expo-router test

---
+++ app/group.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+
+export default function mainScreen(){
+  return (
+    <View>
+      <Text>group</Text>
+    </View>
+  )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
+import { Link } from 'expo-router';
+
 export default function mainScreen(){
   return (
     <View style={styles.container}>
       <Text>test</Text>
+      <Link href="/group">go to group</Link>
     </View>
   )
 }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#f0fff0',
-      alignItems: 'center', //horizontal center
-      justifyContent: 'center', //straight center
-      paddingVertical: 20
-      
-    }
-  });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0fff0',
+    alignItems: 'center', //horizontal center
+    justifyContent: 'center', //straight center
+    paddingVertical: 20
+    
+  }
+});
