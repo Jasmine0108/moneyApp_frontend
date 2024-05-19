1f4ea60ac3bb36b660c195f47b6f24689f2b3b28
Jasmine <a0905373664@gmail.com>
2024-05-19 21:39:37 +0800

    set tamagui

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import { Link } from 'expo-router';
+import appConfig from '../tamagui.config'
+import { TamaguiProvider, createTamagui } from 'tamagui' 
+
+// you usually export this from a tamagui.config.ts file
+const tamaguiConfig = createTamagui(appConfig)
+
+// make TypeScript type everything based on your config
+type Conf = typeof tamaguiConfig
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function mainScreen(){
   return (
-    <View style={styles.container}>
-      <Text>test</Text>
-      <Link href="/group">go to group</Link>
-    </View>
+    <TamaguiProvider config={tamaguiConfig}>
+      <View style={styles.container}>
+        <Text>test</Text>
+        <Link href="/group">go to group</Link>
+      </View>
+    </TamaguiProvider>
+    
   )
 }
 const styles = StyleSheet.create({
