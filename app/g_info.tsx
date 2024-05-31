a2ed121f5845f01923ab18b991c68c7ae895b9ee
hunghuachu <chu20738@gmail.com>
2024-05-31 16:26:15 +0800

    update g_info

--- app/g_info.tsx
+++ app/g_info.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, TextInput, StyleSheet, ScrollView, Button, TouchableOpacity } from 'react-native';
 import SectionedMultiSelect from 'react-native-sectioned-multi-select';
 import { Feather } from '@expo/vector-icons';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { getFontSize } from 'tamagui';
+
 const App = () => {
   const [payer, setPayer] = useState([]);
   const [participants, setParticipants] = useState([]);
   const [item, setItem] = useState('');
   const [amount, setAmount] = useState('');
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const [leftNumber, setLeftNumber] = useState(0);
+  const [rightNumber, setRightNumber] = useState(0);
+
+  // 假设有一些函数来更新这些数字
+  const updateLeftNumber = (newNumber) => {
+    setLeftNumber(newNumber);
+  };
+
+  const updateRightNumber = (newNumber) => {
+    setRightNumber(newNumber);
+  };
+  
 
   const payerItems = [
     { id: 'payer1', name: '付款人1' },
@@ -19,38 +36,58 @@ const App = () => {
     { id: 'participant2', name: '分帳者2' },
   ];
 
-  // Function to handle changes in the payer selection
   const onPayerChange = (selectedPayer) => {
     setPayer(selectedPayer);
   };
 
-  // Function to handle changes in the participants selection
   const onParticipantsChange = (selectedParticipants) => {
     setParticipants(selectedParticipants);
   };
 
-  // Icon component to render Feather icons
-  const Icon = ({ name, size, style }) => (
-    <Feather name={name} size={size} style={style} />
-  );
+  const IconRenderer = ({ name, style }) => {
+    return <Feather name="x-circle" size={15} style={[styles.icon, style]} />;
+  };
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setShow(false);
+    setDate(currentDate);
+    
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
+  };
+  
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
-        {/* Top Section */}
+        {/* 頂部 */}
         <View style={styles.topSection}>
           <View style={styles.side}>
-            <Text style={styles.numberText}>1</Text>
+            <Text style={{ fontSize: 30 }}>{leftNumber}</Text>
+          </View>
+          <View style={styles.top_iconContainer}>
+              <Feather name="users" size={20} style={styles.icon}  />
           </View>
           <View style={styles.verticalLine} />
           <View style={styles.side}>
-            <Text style={styles.numberText}>2</Text>
+            <Text style={{ fontSize: 30 }}>{rightNumber}</Text>
           </View>
+          <View style={styles.top_iconContainer}>
+              <Feather name="user" size={20} style={styles.icon} />
+          </View>
+          
         </View>
 
-        {/* 中間 Section */}
+        {/* 中間 */}
         <View style={styles.middleSection}>
-          {/* Item Input */}
+          {/* 品項輸入 */}
           <View style={styles.inputContainer}>
             <View style={styles.iconContainer}>
               <Feather name="clipboard" size={20} style={styles.icon} />
@@ -58,13 +95,13 @@ const App = () => {
             <TextInput
               style={styles.input}
               placeholder="品項"
-              placeholderTextColor={"black"}
+              placeholderTextColor="black"
               value={item}
               onChangeText={setItem}
             />
           </View>
 
-          {/* Amount Input */}
+          {/* 金額輸入 */}
           <View style={styles.inputContainer}>
             <View style={styles.iconContainer}>
               <Feather name="dollar-sign" size={20} style={styles.icon} />
@@ -72,104 +109,84 @@ const App = () => {
             <TextInput
               style={styles.input}
               placeholder="金額"
-              placeholderTextColor={"black"}
+              placeholderTextColor="black"
               value={amount}
               keyboardType="numeric"
               onChangeText={setAmount}
             />
           </View>
 
-          {/* Payer Selection */}
+          {/* 付款人 */}
           <View style={styles.inputContainer}>
             <View style={styles.iconContainer}>
               <Feather name="users" size={20} style={styles.icon} />
             </View>
-            <SectionedMultiSelect
-              items={payerItems}
-              uniqueKey="id"
-              selectText="選擇支付者"
-              searchPlaceholderText="Search Payer"
-              onSelectedItemsChange={onPayerChange}
-              selectedItems={payer}
-              styles={{
-                selectToggle: {
-                  width: '80%', // 設置寬度以匹配其他輸入框
-                  backgroundColor: 'white', // 背景色
-                  borderColor: 'gray', // 邊框顏色
-                  borderWidth: 1, // 邊框寬度
-                  borderRadius: 5, // 圓角
-                  paddingHorizontal: 10, // 水平內邊距
-                  height: 40, // 高度
-                  marginBottom: 10, // 底部間距
-                },
-                // itemText: {
-                //   color: 'black', // 選項文字顏色
-                // },
-                // selectedItemText: {
-                //   color: 'black', // 已選項目文字顏色
-                // },
-                // confirmText: {
-                //   color: 'blue', // 確認按鈕文字顏色
-                // },
-              }}
-              IconRenderer={Icon}
-            />
+            <View style={styles.multiSelectContainer}>
+              <SectionedMultiSelect
+                items={payerItems}
+                uniqueKey="id"
+                displayKey="name"
+                selectedItems={payer}
+                onSelectedItemsChange={onPayerChange}
+                single={false}
+                searchPlaceholderText="搜尋"
+                selectText="選擇付款人"
+                IconRenderer={IconRenderer}
+              />
+            </View>
           </View>
 
-          {/* Participants Selection */}
+          {/* 分帳者 */}
           <View style={styles.inputContainer}>
             <View style={styles.iconContainer}>
               <Feather name="users" size={20} style={styles.icon} />
             </View>
-            <SectionedMultiSelect
-              items={participantItems}
-              uniqueKey="id"
-              selectText="選擇分帳者"
-              searchPlaceholderText="Search Participants"
-              onSelectedItemsChange={onParticipantsChange}
-              selectedItems={participants}
-              styles={{
-                selectToggle: {
-                  width: '80%', // 設置寬度以匹配其他輸入框
-                  backgroundColor: 'white', // 背景色
-                  borderColor: 'gray', // 邊框顏色
-                  borderWidth: 1, // 邊框寬度
-                  borderRadius: 5, // 圓角
-                  paddingHorizontal: 10, // 水平內邊距
-                  height: 40, // 高度
-                  marginBottom: 10, // 底部間距
-                },
-                // itemText: {
-                //   color: 'black', // 選項文字顏色
-                // },
-                // selectedItemText: {
-                //   color: 'black', // 已選項目文字顏色
-                // },
-                // confirmText: {
-                //   color: 'blue', // 確認按鈕文字顏色
-                // },
-              }}
-              IconRenderer={Icon}
-            />
+            <View style={styles.multiSelectContainer}>
+              <SectionedMultiSelect
+                items={participantItems}
+                uniqueKey="id"
+                displayKey="name"
+                selectedItems={participants}
+                onSelectedItemsChange={onParticipantsChange}
+                single={false}
+                searchPlaceholderText="搜尋"
+                selectText="選擇分帳者"
+                IconRenderer={IconRenderer}
+              />
+            </View>
           </View>
 
-          {/* Date Input */}
+          {/* 日期輸入 */}
           <View style={styles.inputContainer}>
             <View style={styles.iconContainer}>
               <Feather name="calendar" size={20} style={styles.icon} />
             </View>
-            <TextInput
-              style={styles.input}
-              placeholder="Date"
-              value={date}
-              onChangeText={setDate}
-            />
+            <View style={styles.multiSelectContainer}>
+              <Button onPress={showDatepicker} title="Show date picker!" />
+              {show && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  onChange={onChange}
+                  
+                />
+              )}
+              <Text> {String(date)}</Text>
+            </View>
+            
+            {/* <Text>selected: {date.toLocaleString()}</Text> */}
+          </View>
+          <View>
+          <TouchableOpacity style={styles.button} >
+            <Text>確定</Text>
+          </TouchableOpacity>
           </View>
         </View>
 
-        {/* Bottom Section */}
+        {/* 底部部分 */}
         <View style={styles.bottomSection}>
-          <Text style={styles.bottomText}>Bottom Section</Text>
+          <Text style={styles.bottomText}>帳單</Text>
+          <View style={styles.horizontalLine} />
         </View>
       </View>
     </ScrollView>
@@ -186,22 +203,37 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   topSection: {
+    flex: 1,
     flexDirection: 'row',
+    height: 100,
     backgroundColor: '#E0E0E0',
-    width: '80%',
+    justifyContent: 'flex-end',
+    //alignItems: 'flex-end',
     marginBottom: 10,
+    borderRadius: 10,
   },
   side: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+   
+  },
+  top_iconContainer: {
+    marginRight: 10,
+    marginBottom: '5%',
+    alignSelf: 'flex-end',
   },
   verticalLine: {
     width: 1,
     backgroundColor: 'black',
+    height: '90%',
+    alignSelf: 'center',
   },
-  numberText: {
-    fontSize: 24,
+  horizontalLine: {
+    height: 1,
+    backgroundColor: 'black',
+    alignSelf: 'stretch',
+    marginVertical: 10,
   },
   middleSection: {
     flex: 1,
@@ -209,15 +241,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     marginBottom: 10,
+    borderRadius: 10,
   },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'baseline',
     marginBottom: 10,
-    width: '80%'
+    width: '90%',
   },
   iconContainer: {
     marginRight: 10,
+    
   },
   icon: {
     color: 'black',
@@ -228,16 +262,47 @@ const styles = StyleSheet.create({
     width: '80%',
     paddingHorizontal: 10,
     backgroundColor: 'white',
+    borderRadius: 4,
   },
-
+  multiSelectContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 4,
+    padding: 5,
+    
+  },
+  dateInput: {
+    flex: 1,
+    height: 40,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    backgroundColor: 'white',
+    borderRadius: 4,
+  },
+  dateText: {
+    color: 'black',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 10,
+    width: 100,
+  },
   bottomSection: {
     flex: 1,
     backgroundColor: '#E0E0E0',
     justifyContent: 'center',
-    alignItems: 'center',
+   // alignItems: 'center',
+    borderRadius: 10,
   },
   bottomText: {
     fontSize: 18,
+    flexDirection: 'row',
+    alignItems: 'flex-start', // 对齐到顶部
+    justifyContent: 'flex-start', // 对齐到左侧
+    marginLeft: 20,
   },
 });
 
