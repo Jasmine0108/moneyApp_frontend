257c66417661068fcf889c2f582676c05ffb6498
Paul1336 <paul.k.c1336@gmail.com>
2024-06-20 18:43:05 +0800

    feat: balances summarized, (WIP): connect to backend and sorting

--- app/group_balance.tsx
+++ app/group_balance.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
-import { View, Button, Dialog, XStack, YStack } from 'tamagui'
+import {
+  View,
+  Button,
+  Dialog,
+  XStack,
+  YStack,
+  SizableText,
+  Tabs,
+} from 'tamagui'
 import { Link } from 'expo-router'
 import { Colors } from '../constants/Colors'
 import { useRouter } from 'expo-router'
@@ -8,38 +16,55 @@ import AuthService from '../services/auth/auth'
 import GroupService from '../services/group/group'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useIsFocused } from '@react-navigation/native'
+import { useState } from 'react'
 
+interface balances {
+  name: string
+  balance: number
+}
+
+interface transfers {
+  from: string
+  to: string
+  amount: number
+}
+
+const exampleBalances: balances[] = [
+  { name: 'User1', balance: 800 },
+  { name: 'User2', balance: 500 },
+  { name: 'User3', balance: 200 },
+  { name: 'User4', balance: -800 },
+  { name: 'User5', balance: -500 },
+  { name: 'User6', balance: -200 },
+]
+const exampleTransfer: transfers[] = [
+  { from: 'User1', to: 'User1', amount: 800 },
+  { from: 'User2', to: 'User1', amount: 500 },
+  { from: 'User3', to: 'User1', amount: 200 },
+  { from: 'User4', to: 'User1', amount: 800 },
+  { from: 'User5', to: 'User2', amount: 500 },
+  { from: 'User6', to: 'User1', amount: 200 },
+]
 export default function groupContentScreen() {
-  const [groupName, setGroupName] = React.useState('')
-  const [accessToken, setAccessToken] = React.useState('')
-  const [groupId, setGroupId] = React.useState('')
-  const [bills, setBills] = React.useState('')
-
+  const [groupName, setGroupName] = useState('')
+  const [myUserName, setMyUserName] = useState('User2')
+  const [accessToken, setAccessToken] = useState('')
+  const [groupId, setGroupId] = useState('')
+  const [bills, setBills] = useState('')
+  const [activeTab, setActiveTab] = useState('group')
+  const [groupBalances, setGroupBalances] =
+    useState<balances[]>(exampleBalances)
+  const [personTransfers, setPersonTransfers] =
+    useState<transfers[]>(exampleTransfer)
   const router = useRouter()
   const isFocused = useIsFocused()
-  const handleInsertBill = async () => {
-    //console.log('userId:', await AsyncStorage.getItem('@userId'))
-    try {
-      var obj = {
-        totalMoney: 33,
-        title: 'test_062001',
-        description: 'test_062001_description',
-        prepaidPeople: [
-          { memberId: 'a5473f1c-547a-4ffb-8a81-493f2a5e1dc6', amount: 33 },
-        ],
-        splitPeople: [
-          { memberId: 'a5473f1c-547a-4ffb-8a81-493f2a5e1dc6', amount: 33 },
-        ],
-      }
-      GroupService.modifyBills(
-        accessToken,
-        '3949e5db-5b62-48a9-9d5e-ce7fb633bf10',
-        obj
-      )
-    } catch (e) {
-      console.log(e)
-    }
+  const handleButtonClick = async (action: string) => {
+    router.navigate('/group_content')
   }
+  const handleTabSwitch = (tab: string) => {
+    setActiveTab(tab)
+  }
+
   React.useEffect(() => {
     const getGroupInfo = async () => {
       try {
@@ -51,12 +76,9 @@ export default function groupContentScreen() {
         console.log(e)
       }
       var res = await AuthService.getGroupInfo(accessToken, groupId)
-      console.log('res_info', res)
+      //console.log('res_info', res)
       setGroupName(res.name)
     }
-    if (isFocused) getGroupInfo()
-  }, [isFocused])
-  React.useEffect(() => {
     const getBills = async () => {
       try {
         var accessToken = await AsyncStorage.getItem('@accessToken')
@@ -67,18 +89,198 @@ export default function groupContentScreen() {
         console.log(e)
       }
       var res = await GroupService.getBills(accessToken, groupId)
-      console.log('bill_info', res)
+      //console.log('bill_info', res)
       setBills(res)
     }
-    if (isFocused) getBills()
-    console.log(bills)
+    if (isFocused) {
+      getGroupInfo()
+      getBills()
+    }
+    console.log('bills:', bills)
   }, [isFocused])
 
   return (
-    <View bg={Colors.bg} alignItems="center" justifyContent="center" flex={1}>
-      <Link href="/group">return</Link>
-      <Link href="/group_balance">結餘</Link>
-      <Button onPress={() => handleInsertBill()}>test</Button>
+    <View
+      bg={Colors.bg}
+      alignItems="center"
+      height="100%"
+      justifyContent="center"
+    >
+      <Tabs
+        defaultValue="group"
+        width="100%"
+        flexDirection="column"
+        paddingHorizontal="35px"
+      >
+        <Tabs.List>
+          <Tabs.Tab
+            value="group"
+            flex={1}
+            borderWidth="0px"
+            borderTopLeftRadius="20px"
+            borderBottomLeftRadius="0px"
+            borderBottomRightRadius="0px"
+            borderTopRightRadius="20px"
+            onPress={() => handleTabSwitch('group')}
+            {...(activeTab === 'group'
+              ? { background: '#E0DDD6' }
+              : {
+                  background: '#FAFAF4',
+                  borderColor: '#E0DDD6',
+                  borderWidth: '$-10',
+                })}
+          >
+            <SizableText>群組帳務</SizableText>
+          </Tabs.Tab>
+          <Tabs.Tab
+            value="person"
+            flex={1}
+            background={Colors.primary}
+            borderWidth="0px"
+            borderTopLeftRadius="20px"
+            borderTopRightRadius="20px"
+            borderBottomLeftRadius="0px"
+            borderBottomRightRadius="0px"
+            onPress={() => handleTabSwitch('person')}
+            {...(activeTab === 'person'
+              ? { background: '#E0DDD6' }
+              : {
+                  background: '#FAFAF4',
+                  borderColor: '#E0DDD6',
+                  borderWidth: '$-10',
+                })}
+          >
+            <SizableText>個人帳務</SizableText>
+          </Tabs.Tab>
+        </Tabs.List>
+
+        <Tabs.Content value="group">
+          {groupBalances &&
+            Object.keys(groupBalances).map((i, index) => (
+              <View
+                height="52px"
+                width="100%"
+                flexDirection="row"
+                justifyContent="space-around"
+                alignItems="center"
+                {...(index % 2 == 0
+                  ? { backgroundColor: '#FFFFFF' }
+                  : {
+                      backgroundColor: '#E0DDD6',
+                    })}
+                {...(groupBalances.length - 1 == index
+                  ? {
+                      borderBottomLeftRadius: '20px',
+                      borderBottomRightRadius: '20px',
+                    }
+                  : {})}
+              >
+                <SizableText
+                  fontWeight={400}
+                  fontSize="20px"
+                  {...(groupBalances[index].balance < 0
+                    ? { color: '#AA2929' }
+                    : {
+                        color: '#10520E',
+                      })}
+                >
+                  {groupBalances[index].name}
+                </SizableText>
+                <SizableText
+                  fontWeight={400}
+                  fontSize="20px"
+                  {...(groupBalances[index].balance < 0
+                    ? { color: '#AA2929' }
+                    : {
+                        color: '#10520E',
+                      })}
+                >
+                  {groupBalances[index].balance}
+                </SizableText>
+              </View>
+            ))}
+        </Tabs.Content>
+        <Tabs.Content value="person">
+          {personTransfers &&
+            Object.keys(personTransfers).map((i, index) => (
+              <View
+                height="52px"
+                width="100%"
+                flexDirection="row"
+                justifyContent="space-between"
+                alignItems="center"
+                {...(index % 2 == 0
+                  ? { backgroundColor: '#FFFFFF' }
+                  : {
+                      backgroundColor: '#E0DDD6',
+                    })}
+                {...(personTransfers.length - 1 == index
+                  ? {
+                      borderBottomLeftRadius: '20px',
+                      borderBottomRightRadius: '20px',
+                    }
+                  : {})}
+              >
+                <View flexDirection="row" paddingHorizontal="20px">
+                  <SizableText
+                    fontWeight={400}
+                    fontSize="20px"
+                    color="#545454"
+                    {...(personTransfers[index].from == myUserName
+                      ? { color: '#AA2929' }
+                      : {})}
+                    {...(personTransfers[index].to == myUserName
+                      ? { color: '#10520E' }
+                      : {})}
+                  >
+                    {personTransfers[index].from}
+                  </SizableText>
+                  <SizableText
+                    fontWeight={400}
+                    fontSize="20px"
+                    {...(personTransfers[index].from == myUserName
+                      ? { color: '#AA2929' }
+                      : {})}
+                    {...(personTransfers[index].to == myUserName
+                      ? { color: '#10520E' }
+                      : {})}
+                  >
+                    {' -> '}
+                  </SizableText>
+                  <SizableText
+                    fontWeight={400}
+                    fontSize="20px"
+                    {...(personTransfers[index].from == myUserName
+                      ? { color: '#AA2929' }
+                      : {})}
+                    {...(personTransfers[index].to == myUserName
+                      ? { color: '#10520E' }
+                      : {})}
+                  >
+                    {personTransfers[index].to}
+                  </SizableText>
+                </View>
+                <View paddingHorizontal="20px">
+                  <SizableText
+                    fontWeight={400}
+                    fontSize="20px"
+                    {...(personTransfers[index].from == myUserName
+                      ? { color: '#AA2929' }
+                      : {})}
+                    {...(personTransfers[index].to == myUserName
+                      ? { color: '#10520E' }
+                      : {})}
+                  >
+                    {personTransfers[index].amount}
+                  </SizableText>
+                </View>
+              </View>
+            ))}
+        </Tabs.Content>
+      </Tabs>
+      <View alignItems="center" justifyContent="center" paddingTop="50px">
+        <Button onPress={() => handleButtonClick('confirm')}>確定</Button>
+      </View>
     </View>
   )
 }
--- app/group_content.tsx
+++ app/group_content.tsx
@@ -1,140 +1,134 @@
-
 import React from 'react'
-import { View, Button, Dialog, XStack, YStack} from 'tamagui'
+import { View, Button, Dialog, XStack, YStack } from 'tamagui'
 import { Link } from 'expo-router'
 import { Colors } from '../constants/Colors'
 import { useRouter } from 'expo-router'
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons'
 import AuthService from '../services/auth/auth'
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { useIsFocused } from '@react-navigation/native';
-import * as Clipboard from 'expo-clipboard';
+import { useIsFocused } from '@react-navigation/native'
+import * as Clipboard from 'expo-clipboard'
 
-export default function groupContentScreen(){
-    const [groupName, setGroupName] = React.useState('')
-    const [accessToken, setAccessToken] = React.useState('')
-    const [groupId, setGroupId] = React.useState('')
-    const [inviteCode, setInviteCode] = React.useState('')
+export default function groupContentScreen() {
+  const [groupName, setGroupName] = React.useState('')
+  const [accessToken, setAccessToken] = React.useState('')
+  const [groupId, setGroupId] = React.useState('')
+  const [inviteCode, setInviteCode] = React.useState('')
 
-    const router = useRouter()
-    const isFocused = useIsFocused();
-    const copyToClipboard = async() => {
-        
-        try{
-          await Clipboard.setStringAsync(inviteCode);
-        }
-        catch (e) {
-          console.log(e)
-        }
-      };
-     
-      const generateGroupInviteCode = async () => {
-        const res = await AuthService.setGroupInviteCode(accessToken, groupId)
-        console.log('res_invite', res)
-        console.log('inviteCode: ', res.inviteCode)
-        setInviteCode(res.inviteCode)
+  const router = useRouter()
+  const isFocused = useIsFocused()
+  const copyToClipboard = async () => {
+    try {
+      await Clipboard.setStringAsync(inviteCode)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
-
-
+  const generateGroupInviteCode = async () => {
+    const res = await AuthService.setGroupInviteCode(accessToken, groupId)
+    console.log('res_invite', res)
+    console.log('inviteCode: ', res.inviteCode)
+    setInviteCode(res.inviteCode)
+  }
+  React.useEffect(() => {
+    const getGroupInfo = async () => {
+      try {
+        var accessToken = await AsyncStorage.getItem('@accessToken')
+        var groupId = await AsyncStorage.getItem('@currentGroupId')
+        setAccessToken(accessToken)
+        setGroupId(groupId)
+      } catch (e) {
+        console.log(e)
       }
-      React.useEffect(() =>{
-        const getGroupInfo = async() =>{
-            try{
-                var accessToken = await AsyncStorage.getItem('@accessToken')
-                var groupId = await AsyncStorage.getItem('@currentGroupId')
-                setAccessToken(accessToken)
-                setGroupId(groupId)
-            }
-            catch(e){
-              console.log(e)
-            }
-            var res = await AuthService.getGroupInfo(accessToken, groupId)
-            console.log('res_info', res)
-            setGroupName(res.name)
-        }
-        if(isFocused)
-            getGroupInfo()
-    },[isFocused])
+      var res = await AuthService.getGroupInfo(accessToken, groupId)
+      console.log('res_info', res)
+      setGroupName(res.name)
+    }
+    if (isFocused) getGroupInfo()
+  }, [isFocused])
 
-   
-    
-    return(
-        <View bg={Colors.bg} alignItems="center" justifyContent="center" flex={1}>   
-        <Dialog modal>
-          <Dialog.Trigger asChild>
-            <Button onPress={generateGroupInviteCode}>Show room number</Button>
-          </Dialog.Trigger>
-          <Dialog.Portal>
-            <Dialog.Overlay
-              key="overlay"
-              animation="quick"
-              opacity={0.5}
-              enterStyle={{ opacity: 0 }}
-              exitStyle={{ opacity: 0 }}
-            />
-            <Dialog.Content
-              bg={Colors.primary}
-              height="23%"
-              width="80%"
-              bordered
-              elevate
-              key="content"
-              animation={[
-                'quick',
-                {
-                  opacity: {
-                    overshootClamping: true,
-                  },
+  return (
+    <View bg={Colors.bg} alignItems="center" justifyContent="center" flex={1}>
+      <Dialog modal>
+        <Dialog.Trigger asChild>
+          <Button onPress={generateGroupInviteCode}>Show room number</Button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay
+            key="overlay"
+            animation="quick"
+            opacity={0.5}
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+          <Dialog.Content
+            bg={Colors.primary}
+            height="23%"
+            width="80%"
+            bordered
+            elevate
+            key="content"
+            animation={[
+              'quick',
+              {
+                opacity: {
+                  overshootClamping: true,
                 },
-              ]}
-              enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
-              exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
-              x={0}
-              scale={1}
-              opacity={1}
-              y={0}
-            >
-          <YStack alignItems="center" justifyContent="center">
-            <Dialog.Title color={Colors.text} fontSize={20}>
-              {groupName}
-            </Dialog.Title>
-            <View height="7%" />
-            <XStack height="30%" alignItems="center" >
-            <Dialog.Title color={Colors.text} fontSize={20}>
-              房號
-            </Dialog.Title>
-            <View width="5%" />
-            <View bg={Colors.input_bg} width="35%" height="100%" alignItems="center" justifyContent="center" px="1%">
-          
-            <Dialog.Description color={Colors.text}>
-              {inviteCode}
-            </Dialog.Description>
-            
-            </View>
-            <Button bg={Colors.primary} onPress={copyToClipboard}>
-            <FontAwesome5 name="copy" size={24} color="black"/>
-          </Button> 
-            </XStack>
-            <View height="10%" />
-            <Dialog.Close displayWhenAdapted asChild>
-            <Button 
-                theme="active" 
-                aria-label="Close"
-                bg={Colors.button_primary} 
-                borderRadius={20}
-                width="40%"
-            >
-                close
-            </Button>
-             
-            </Dialog.Close>
-          </YStack>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog>
-            
-            <Link href="/group">return</Link>
-        </View>
-    )
-    
+              },
+            ]}
+            enterStyle={{ x: 0, y: -20, opacity: 0, scale: 0.9 }}
+            exitStyle={{ x: 0, y: 10, opacity: 0, scale: 0.95 }}
+            x={0}
+            scale={1}
+            opacity={1}
+            y={0}
+          >
+            <YStack alignItems="center" justifyContent="center">
+              <Dialog.Title color={Colors.text} fontSize={20}>
+                {groupName}
+              </Dialog.Title>
+              <View height="7%" />
+              <XStack height="30%" alignItems="center">
+                <Dialog.Title color={Colors.text} fontSize={20}>
+                  房號
+                </Dialog.Title>
+                <View width="5%" />
+                <View
+                  bg={Colors.input_bg}
+                  width="35%"
+                  height="100%"
+                  alignItems="center"
+                  justifyContent="center"
+                  px="1%"
+                >
+                  <Dialog.Description color={Colors.text}>
+                    {inviteCode}
+                  </Dialog.Description>
+                </View>
+                <Button bg={Colors.primary} onPress={copyToClipboard}>
+                  <FontAwesome5 name="copy" size={24} color="black" />
+                </Button>
+              </XStack>
+              <View height="10%" />
+              <Dialog.Close displayWhenAdapted asChild>
+                <Button
+                  theme="active"
+                  aria-label="Close"
+                  bg={Colors.button_primary}
+                  borderRadius={20}
+                  width="40%"
+                >
+                  close
+                </Button>
+              </Dialog.Close>
+            </YStack>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog>
+
+      <Link href="/group">return</Link>
+      <Link href="/group_balance">結餘</Link>
+    </View>
+  )
 }
